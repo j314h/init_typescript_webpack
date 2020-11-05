2f39bd8b8cc157b0d341a6583a1ab3b717149e13
j314h <johnhaimez314@gmail.com>
2020-11-05 14:40:23 +0100

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# typescript webpack basic
+
+- Files install of basic
+
+        npm init -y
+        npm i typescript webpack webpack-cli webpack-dev-server ts-loader html-webpack-plugin
+
+        1 package.json
+        => build = webpack
+        => start = webpack-dev-server
+
+        1 tsconfig.json
+        => config this loader = loader ts
+
+        1 webpack.config.json
+        => config of webpack
+        => enter file, out file, give path of files
+        => init loader
+        => init plugin
+        => init properties server of test developper
+
+* install
+
+  - execute `npm i` in terminal for install dependency
+  - execute `npm run build` for create and generate bundle of project
+  - execute `npm run start` for see result in browser
+  - enjoye
+
+* warning
+
+  - delete remote of this project github for developp your personal project
+
+    - command `git remote rm nameofremote (origin or any remote of your choice)`
+    - **forbidden** write and push this branch and project
+    - verified version dependency of webpack if problem
+
+> basic configuration, you can change if you have specific need.
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// for path of properties in this document
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // file dote entry for analyse of code
+  // this import in file beacame dependency of file
+  entry: {
+    main: path.join(__dirname, 'src/index.ts'),
+  },
+  // file and folder build exit
+  // create bundle whith name of entry (main)
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  // loader divers of dependency tree
+  // loader of divers format files
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        exclude: /(node_modules)/,
+        use: ['ts-loader'],
+      },
+    ],
+  },
+  // plugin divers
+  // here run this assets (img, movie, ...)
+  // this create file html with bundle of files generate
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+    }),
+  ],
+  // dev server
+  stats: 'minimal',
+  devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    open: false,
+    contentBase: './dist',
+    inline: true,
+    port: 5000,
+  },
+};
